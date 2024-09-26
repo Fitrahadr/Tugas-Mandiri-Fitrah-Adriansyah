676e67a252005b3243381f51f66637c164547272
Fitrahadr <kakafitrah75@gmail.com>
2024-09-26 19:03:47 +0700

    Add files via upload

---
+++ Fitrah_Adriansyah[1].cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main ()
+{
+	printf("Panjang = 10 cm\n");
+	printf("Lebar = 7 cm\n");
+	
+	int P, b, L;
+	P = 10;
+	b = 7;
+	L = P * b;
+	printf("%i", L);
+	return 0;
+}
---
+++ Tugas_Mandiri_No.1_Fitrah_Adriansyah[1].cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main ()
+{
+	printf("NAMA : Fitrah Adriansyah\n");
+	printf("NIM : 2441018\n");
+	printf("JURUSAN : Teknik Informatika\n");
+	
+	return 0;
+}
